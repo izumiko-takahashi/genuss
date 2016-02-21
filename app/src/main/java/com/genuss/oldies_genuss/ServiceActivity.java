e90491b7ed9320e046083ab5c1f17018a0a05ff9
Yarely Alvarez <yarely@appetito.mx>
2016-02-20 23:42:12 -0600

    Agrega los cambios en ServiceActivity

---
+++ app/src/main/java/com/genuss/oldies_genuss/ServiceActivity.java
@@ -0,0 +1,23 @@
+package com.genuss.oldies_genuss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 20/02/2016.
+ */
+public class ServiceActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanteState)
+    {
+        super.onCreate(savedInstanteState);
+
+
+    }
+    @Override void onCreateView(View view)
+    {
+
+
+    }
+
+}
